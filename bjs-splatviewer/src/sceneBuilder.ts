829d3e84c0a9118d85d0e1e1ee9421ead64641ad
noname <48761044+noname0310@users.noreply.github.com>
2024-02-12 04:57:42 +0900

    optimize scene

--- bjs-splatviewer/src/sceneBuilder.ts
+++ bjs-splatviewer/src/sceneBuilder.ts
@@ -14,13 +14,14 @@ import type { ISceneBuilder } from "./baseRuntime";
 export class SceneBuilder implements ISceneBuilder {
     public async build(_canvas: HTMLCanvasElement, engine: Engine): Promise<Scene> {
         const scene = new Scene(engine);
+        scene.clearColor.set(0.8, 0.8, 0.8, 1);
 
         const cameraInitialTarget = Vector3.Zero();
         const cameraInitialPosition = new Vector3(0, 1, -5);
 
         const camera = new ArcRotateCamera("camera1", 0, 0, 45, cameraInitialTarget, scene);
         camera.setPosition(cameraInitialPosition);
-        camera.attachControl(undefined, false);
+        camera.attachControl(undefined, true);
         camera.inertia = 0.8;
         camera.wheelDeltaPercentage = 0.01;
         camera.lowerRadiusLimit = 2;
@@ -38,6 +39,25 @@ export class SceneBuilder implements ISceneBuilder {
                 camera.setPosition(cameraInitialPosition);
             }
         });
+
+        scene.freezeMaterials();
+
+        const meshes = scene.meshes;
+        for (let i = 0, len = meshes.length; i < len; ++i) {
+            const mesh = meshes[i];
+            mesh.freezeWorldMatrix();
+            mesh.doNotSyncBoundingInfo = true;
+            mesh.isPickable = false;
+            mesh.alwaysSelectAsActiveMesh = true;
+        }
+
+        scene.skipPointerMovePicking = true;
+        scene.skipPointerDownPicking = true;
+        scene.skipPointerUpPicking = true;
+        scene.skipFrustumClipping = true;
+        scene.blockMaterialDirtyMechanism = true;
+        scene.clearCachedVertexData();
+        scene.cleanCachedTextureBuffer();
 
         return scene;
     }
